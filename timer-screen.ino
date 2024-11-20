1480bcce9a7dfbc71d9ea99b6f30a21238480d91
Murakass <117369244+Murakass@users.noreply.github.com>
2024-11-20 16:35:12 +0200

    Functioning timer
    Moved the timer to a function
    Made the timer display minutes and seconds in a --:-- format

--- timer-screen.ino
+++ timer-screen.ino
@@ -8,33 +8,78 @@ Timer timer;
 
 U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C u8g2(U8G2_R0); 
 
- void setup(void) {
-    u8g2.begin();
-    Serial.begin(9600);    
+void setup(void) {
+  u8g2.begin();
+  Serial.begin(9600);    
 }
 
-int input = 15;
 
-  void loop(void) {
-    timer.start();
-    if(timer.state() == RUNNING) Serial.println("timer running");
+int input = 5;
 
-    for(int i=0; i <= input; i++){
+int timerOLED(int input){
+  timer.start();
+  if(timer.state() == RUNNING) Serial.println("timer running");
 
-      int time = (timer.read()/1000);
-      char cstr[16];
-      itoa(time, cstr, 10);
-      Serial.println(cstr);
-      u8g2.clearBuffer();					// clear the internal memory
-      u8g2.setFont(u8g2_font_logisoso28_tr);  // choose a suitable font at https://github.com/olikraus/u8g2/wiki/fntlistall
-      u8g2.drawStr(8,29,cstr);	// write something to the internal memory
-      u8g2.sendBuffer();					// transfer internal memory to the display
-      delay(1000);
+  for(int i=0; i <= input; i++){
+
+    int sec = (input - timer.read()/1000);
+    int min = (sec/60);
+    int secDisplay = (sec-min*60);
+    char cstrSec[5];
+    char cstrMin[5];
+    char cstrSecDisplay[5];
+    char minSec[4];
+    char nullTaht[1] = "0";
+    char koolon[1] = ":";
+    
+    for(int i=0; i<6; i++){
+      minSec[i] = 0;
     }
+    
+    itoa(sec, cstrSec, 10);
+    itoa(min, cstrMin, 10);
+    itoa(secDisplay, cstrSecDisplay, 10);
 
-    timer.stop();
-    if(timer.state() == STOPPED) Serial.println("timer stopped");
+    
+    if(min>=10){
+      minSec[0]=cstrMin[0];
+      minSec[1]=cstrMin[1];
+    }
+    if(min<10){
+      minSec[0]=nullTaht[0];
+      minSec[1]=cstrMin[0];
+    }
+    
 
-   delay(3000);
+    minSec[2]= koolon[0];
+
+    
+    if(secDisplay>=10){
+      minSec[3]=cstrSecDisplay[0];
+      minSec[4]=cstrSecDisplay[1];
+    }
+    if(secDisplay<10){
+      minSec[3]=nullTaht[0];
+      minSec[4]=cstrSecDisplay[0];
+    }
+    
+    
+    Serial.println(minSec);
+    u8g2.clearBuffer();					// clear the internal memory
+    u8g2.setFont(u8g2_font_logisoso28_tr);  // choose a suitable font at https://github.com/olikraus/u8g2/wiki/fntlistall
+    u8g2.drawStr(8,29,minSec);	// write something to the internal memory
+
+    u8g2.sendBuffer();					// transfer internal memory to the display
+    delay(1000);
+  }
+
+  timer.stop();
+  if(timer.state() == STOPPED) Serial.println("timer stopped");
+
+  return 0;
+}
+
+void loop(void) {
+  timerOLED(input);
 }
 
